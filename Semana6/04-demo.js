39852723ca1a7a6df4d5d37383b99e33519d4490
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-03 22:22:43 -0500

    avance

---
+++ Semana6/04-demo.js
@@ -0,0 +1,10 @@
+//calcular el área de un triángulo
+//Necesitamos
+//base
+//altura
+
+let base = prompt("Ingrese la base del triángulo");
+console.log(base);
+console.log(typeof base);
+let altura = prompt('Ingrese la altura del triángulo');
+console.log(altura);
