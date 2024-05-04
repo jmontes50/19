59c486a0a330091ad9136a7ef65a982791367ae8
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-03 22:31:46 -0500

    avance

--- Semana6/04-demo.js
+++ Semana6/04-demo.js
@@ -2,9 +2,25 @@
 //Necesitamos
 //base
 //altura
+/**
+ * let base = parseFloat(prompt("Ingrese la base del triángulo"));
+ * let base = +prompt("Ingrese la base del triángulo");
+ */
 
 let base = prompt("Ingrese la base del triángulo");
 console.log(base);
 console.log(typeof base);
 let altura = prompt('Ingrese la altura del triángulo');
 console.log(altura);
+
+//parsear lo podemos entender como traducir
+//parseFloat convierte el argumento recibido a un number
+let numBase = parseFloat(base);
+console.log(numBase)
+console.log(typeof numBase)
+let numAltura = parseFloat(altura);
+
+let area = numAltura * numBase / 2;
+
+// console.log(area);
+alert(`El área es ${area}`);
