8e94b4e250c7db4f0c4ada98b4948bb1afa14a78
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-03 20:06:59 -0500

    added

--- Semana6/01-javascript.js
+++ Semana6/01-javascript.js
@@ -18,3 +18,33 @@ console.log(edad);
 let precioSmartphone = 900;
 // sensible a mayúsculas
 console.log(precioSmartphone);
+//VAR no lo usen
+var apellido = "Borjas";
+console.log(apellido);
+var apellido = "Soles";
+console.log(apellido);
+
+// let nombre = "Harold";
+//TIPOS DE DATOS;
+//texto - string
+let nombre2 = "Giancarlo";
+//number
+let numero =  20;
+//string
+let nota = "20";
+
+console.log("number", numero);
+console.log("typeof number", typeof numero);
+console.log("string", nota);
+console.log("typeof string", typeof nota);
+//tener cuidado al concatenar o sumar
+// console.log(numero + nota);
+
+//boolean - booleanos
+let meQuiere = true; //false
+console.log("boolean", meQuiere);
+console.log("typeof boolean", typeof meQuiere);
+
+//Le podemos dar más de un argumento
+//puedo hacer que imprima una cosa detrás de otra
+console.log("este numero es:", 100)
