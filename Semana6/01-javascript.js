35b98ffbfad2d569c6639505a8e9687af1b0e7f3
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-03 19:45:30 -0500

    avance

---
+++ Semana6/01-javascript.js
@@ -0,0 +1,20 @@
+//este es un comentario de una línea
+/**
+ * este es un comentario de bloque
+ * aquí puede describir el console.log
+ * en varias lineas
+ */
+console.log("Hola mundo!!!!");
+
+let nombre = "Pamela";
+
+console.log(nombre);
+
+let edad = 25;
+
+console.log(edad);
+// camelCase 🐫, anioDeNacimiento
+// usualmente tíldes o ñ no se van a usar
+let precioSmartphone = 900;
+// sensible a mayúsculas
+console.log(precioSmartphone);
