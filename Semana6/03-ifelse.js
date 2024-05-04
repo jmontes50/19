bb6c373000c8008b63db537a6372986f803c7952
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-03 22:04:40 -0500

    avance

--- Semana6/03-ifelse.js
+++ Semana6/03-ifelse.js
@@ -15,10 +15,33 @@ console.log(10 > 5);
  * === estrictamente igual que
  */
 
+/*
 if (1 > 5){ //evalua esto y si es verdadero...
     //...ejecutas lo que esta dentro de estas llaves
     console.log("es verdadero");
 } else {//pero si no es verdadero entonces ...
     //...ejecutas lo que esta dentro de estas otras llaves
     console.log("es falso");
+}
+*/
+
+console.log("a: ", 9 == 9);
+
+//que sucede? el == compara valores, NO EL TIPO DE DATO;
+console.log("b: ", 10 == "10");
+
+//compara, estrictamente igual, tanto valor, como tipo de dato
+//Siempre comparar de forma estricta
+console.log("c: ", 20 === "20");
+
+// if( 9 === 9 ){
+//     console.log("es verdadero")
+// }
+
+let nombre = "Jorge";
+//length === longitud
+console.log(nombre.length);
+console.log(typeof nombre.length)
+if(nombre.length > 0){
+    console.log("Es un nombre válido");
 }
