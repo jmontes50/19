2d047fa9514f748ab30e2a374b75c9c1d639cc3f
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-08 22:24:02 -0500

    avance

---
+++ Semana6/11-arreglos.js
@@ -0,0 +1,18 @@
+let estudiante1 = "Renzo";
+let estudiante2 = "Roy";
+let estudiante3 = "Harold";
+
+let arrEstudiantes = ["Renzo","Roy","Harold"];
+
+console.log(arrEstudiantes);
+
+console.log(arrEstudiantes[2]);
+
+console.log(arrEstudiantes.length);
+
+console.log(typeof arrEstudiantes.length);
+
+let arr = [100, true, "Hola!", ["Hi","Arr"]];
+
+console.log(arr);
+
