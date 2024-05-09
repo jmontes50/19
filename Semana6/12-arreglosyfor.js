142a1c6bf5e2490ccba4189fe450fa4f57b618bb
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-08 22:34:45 -0500

    avance

---
+++ Semana6/12-arreglosyfor.js
@@ -0,0 +1,9 @@
+let ingredientes = ["Papa seca", "Chancho", "Cebolla", "Ají", "Sal"];
+
+// console.log(ingredientes[2]);
+// for(let i = 0; i <= ingredientes.length - 1; i ++){
+for(let i = 0; i < ingredientes.length; i ++){
+    console.log(ingredientes[i]);
+}
+
+console.log("fin!");
