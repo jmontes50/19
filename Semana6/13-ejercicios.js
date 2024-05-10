7e9315b0dd3e04bf808b23c46c46a941d24070fb
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-09 19:46:13 -0500

    avanc

--- Semana6/13-ejercicios.js
+++ Semana6/13-ejercicios.js
@@ -4,6 +4,7 @@ Tiene un catálogo de productos con estos precios, 50, 20, 10, 100, 30, 10
 Indique cuál es su presupuesto y según el orden en que están indique cuánto gastará Ej. si mi presupuesto es 90 considerare 50, 20 y 10.
 */
 
+/*
 let precios = [50, 20, 10, 100, 30, 10];
 
 let presupuesto = prompt('Cuanto tiene de presupuesto');
@@ -17,3 +18,21 @@ for(let i = 0; i < precios.length; i++){
     numPresupuesto = numPresupuesto - precios[i];
   }
 }
+*/
+
+/**
+ * Según el número ingresado por un usuario calcula la suma de los valores menores a este número comenzando desde 1
+ */
+
+// let numeroIngresado = parseFloat(prompt('Indique un número'))
+let valorIngresado = prompt('Indique un número');
+let numConvertido = parseFloat(valorIngresado);
+let suma = 0;
+
+for(let i = 1; i < numConvertido; i++){
+  // console.log("i:", i);
+  suma = suma + i;
+  // console.log("suma:", suma);
+}
+
+console.log(`La suma es: ${suma}`);
