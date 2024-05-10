c66cc7b4313a9253af18c98888223749b88a42d3
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-09 20:13:13 -0500

    master

--- Semana6/14-breakcontinue.js
+++ Semana6/14-breakcontinue.js
@@ -11,7 +11,7 @@ for(let i = 0; i < nombres.length; i++){
   // console.log(nombres[i]);
 }
 */
-
+/*
 // continue
 let notas = [11, 13, 20, 17, 19, 20, 9, 14];
 
@@ -21,4 +21,28 @@ for(let i = 0; i < notas.length; i++) {
   }
   console.log(`La nota a considerar es ${notas[i]}`)
   //aca abajo tenga mucho más código
+}
+*/
+
+let precios = [50, 20, 10, 100, 30, 10];
+
+let presupuesto = prompt('Cuanto tiene de presupuesto');
+let numPresupuesto = parseFloat(presupuesto);
+
+for(let i = 0; i < precios.length; i++){
+  // confirmacion de compra
+  let continuar = confirm(`Desea comprar el producto de 
+  S/ ${precios[i]}
+  su presupuesto es: S/ ${numPresupuesto}`);
+  // ! NOT negación
+  if(!continuar){
+    continue;
+  }
+  // procesooo
+  if(precios[i] <= numPresupuesto){
+    console.log(`El producto de S/ ${precios[i]} se compra`);
+    //si es que el producto se compra
+    // numPresupuesto -= precios[i];
+    numPresupuesto = numPresupuesto - precios[i];
+  }
 }
