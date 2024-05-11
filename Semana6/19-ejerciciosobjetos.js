9d158dc126c3cf8536b5bef1295826989b891bd7
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-10 20:41:54 -0500

    avance

--- Semana6/19-ejerciciosobjetos.js
+++ Semana6/19-ejerciciosobjetos.js
@@ -19,10 +19,26 @@ let productos = [
 
 //según el arreglo de objetos, muestre los nombres de los productos más caros
 //los más caros mayor a 500
-
+/*
 for (let i = 0; i < productos.length; i++) {
   // console.log(productos[i]);
   if (productos[i].precio > 500) {
     console.log(productos[i].nombre);
   }
 }
+*/
+
+let alumnos = [
+  {
+    nombre: "Esluvia",
+    notas: [18, 14, 19]
+  },
+  {
+    nombre: "Marcia",
+    notas: [17, 14, 20]
+  },
+  {
+    nombre: "Carlos",
+    notas: [13, 19, 17]
+  },
+]
