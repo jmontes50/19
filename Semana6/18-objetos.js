642c319aa3e793778d6bed8b500f4fc7af872007
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-10 20:01:05 -0500

    avance

--- Semana6/18-objetos.js
+++ Semana6/18-objetos.js
@@ -1,18 +1,22 @@
 let gatita = {
-    //propiedad: valor,
-    //key: value
-    nombre: 'Lady',
-    edad: 4,
-    color: "Naranja",
-    esAmigable: false,
-    //método, es una función dentro de un objeto
-    maullar() {
-        console.log('MIAU MIAU MIAU');
-    },
-    comer: function () {
-        console.log("ñam ñam ñam");
-    }
+  //propiedad: valor,
+  //key: value
+  nombre: 'Lady',
+  edad: 4,
+  color: "Naranja",
+  esAmigable: false,
+  //método, es una función dentro de un objeto
+  maullar() {
+    // console.log('MIAU MIAU MIAU');
+    return "miau miau miau";
+  },
+  comer: function () {
+    // console.log("ñam ñam ñam");
+    return "ñam ñam ñam";
+  },
 }
+
+/*
 
 console.log(gatita);
 //si deseamos acceder al valor de 01 sola propiedad
@@ -21,4 +25,29 @@ console.log(gatita.nombre);
 
 console.log(gatita.edad);
 
-gatita.maullar();
+console.log(gatita.maullar());
+
+console.log(gatita.comer());
+
+*/
+
+let deportistas = [
+  {
+    nombre: "Collapse",
+    deporte: "Dota"
+  },
+  {
+    nombre: "Rafael Nadal",
+    deporte: "Tennis"
+  },
+  {
+    nombre: "Cuevita",
+    deporte: "Fútbol"
+  }
+];
+
+console.log(deportistas);
+
+console.log(deportistas[1]);
+
+console.log(deportistas[1].nombre);
