a32f9bbfb6160b5a47ee374263522ae549a5b167
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-15 21:53:28 -0500

    avance

--- Semana7/02-dom.js
+++ Semana7/02-dom.js
@@ -10,11 +10,27 @@ let links = document.getElementsByClassName("menu");
 
 console.log(links);
 
+let arrayLinks = Array.from(links);
+
+console.log("arrayLinks: ", arrayLinks);
+
+arrayLinks.forEach(function (item) {
+  console.log("li: ", item);
+  // con style puedo acceder a diferentes propiedades de css del elemento al que se le este aplicando, ej style.color
+  item.style.color = "blue";
+  item.style.fontFamily = "Arial";
+});
+
 let main = document.querySelector("#principal");
 
 console.log(main)
 
-main.innerHTML = "Hola!!! :D ";
+// main.innerHTML = "Hola!!! :D ";
+main.innerHTML = `
+<article>
+  <h2>Chat GPT anuncio capacidades en audio e imagenes</h2>
+  <p>Texto de relleno</p>
+</article>`
 
 let input = document.getElementById("email");
 console.log("value: ", input.value);
@@ -24,6 +40,9 @@ let button = document.getElementById("enviar");
 //addEventListener
 
 button.addEventListener('click', function () {
-    console.log("valor input", input.value);
+  console.log("valor input", input.value);
+  titulo.style.color = "green";
 })
 
+//propiedad es algo que tiene un valor, string, num, booleano, ...
+//método function
