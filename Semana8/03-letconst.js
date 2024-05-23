690ccfd537cb7d620633e62c056f2b01a4cf73bf
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-22 22:26:40 -0500

    avance

---
+++ Semana8/03-letconst.js
@@ -0,0 +1,17 @@
+let nombre = "Denys";
+
+// let suma = (a, b) => a + b;
+
+// console.log(suma(6, 9));
+
+const IGV = 1.18;
+
+// IGV = 1.15;
+
+const calcularIGV = (costo) => {
+    return costo * IGV;
+}
+
+console.log(calcularIGV(50))
+
+//SI NO VAMOS A MODIFICAR ALGO, declaremoslo con const
