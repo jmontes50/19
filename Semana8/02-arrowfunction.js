bd02c888e3b85db71c05b80fa4a1146569531556
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-22 22:09:25 -0500

    avance

---
+++ Semana8/02-arrowfunction.js
@@ -0,0 +1,23 @@
+// function saludar() {
+//     return "Hola";
+// }
+
+//función anónima
+//ciudadano de primera clase, todo se puede asignar a una variable, INCLUYENDO UNA FUNCIÓN
+// let saludar = function () {
+//     return "Hola";
+// }
+
+//Función flecha
+// let saludar = (nombre) => {
+//     return 'Hola'
+// }
+
+// let saludar = nombre => {
+//     return 'Hola'
+// }
+
+let saludar = nombre => 'Hola'
+
+let saludo = saludar()
+console.log(saludo);
