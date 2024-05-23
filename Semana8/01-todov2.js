1376ac5656de47fe690ea6761427245c6eacd0d5
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-22 20:02:13 -0500

    avance

--- Semana8/01-todov2.js
+++ Semana8/01-todov2.js
@@ -10,9 +10,19 @@ let inputTarea = document.getElementById('nueva-tarea');
 
 let btnTarea = document.getElementById('btn-tarea');
 
+let dataTareas = [];
+
 // quiero escuchar el evento click
 btnTarea.addEventListener('click', function () {
   //Todo es un objeto para JS, incluido los elementos que obtiene del DOM
   //al ser objetos tendran propiedades y métodos útiles.
   console.log(inputTarea.value);
+  // agregarlo en dataTareas
+  // let nuevaTarea = inputTarea.value;
+  let nuevaTarea = {
+    tarea: inputTarea.value,
+    completada: false
+  }
+  dataTareas.push(nuevaTarea);
+  console.table(dataTareas)
 })
