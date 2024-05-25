682e8ff09a09503cfd519f9e13ff8b1cd3dc8d5c
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-24 19:41:43 -0500

    avance

---
+++ Semana8/06-modulos.js
@@ -0,0 +1,10 @@
+//si mi archivo crece demasido necesito separar todo en porciones de codigo más pequeñas
+
+// const calcular = (a, b) => {
+//     return a + b;
+// }
+import { calcular, PI } from "./utils/utilidades.js";
+
+console.log(calcular(10, 50));
+
+console.log(PI)
---
+++ Semana8/utils/utilidades.js
@@ -0,0 +1,11 @@
+const calcular = (a, b) => {
+    return a + b;
+}
+
+const PI = 3.1416
+
+//funciones, variables, objetos, arreglos
+export {
+    calcular,
+    PI
+}
