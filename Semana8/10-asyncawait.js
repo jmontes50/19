a03d34fea3daa861fe4e9649abd2b028a8a3c9b4
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-29 19:45:57 -0500

    avance

--- Semana8/10-asyncawait.js
+++ Semana8/10-asyncawait.js
@@ -1,11 +1,21 @@
+/*
 const ejecutar = () => {
   return new Promise((resolve, reject) => {
-    // resolve("Exito!!!")
-    reject("algo salio mal")
+    resolve("Exito!!!")
+    // reject("algo salio mal")
   });
 };
+*/
 
+//async/await también me permite trabajar con tareas asíncronas
+// async function ejecutar(){}
+const ejecutar = async () => {
+//me va a indicar que hay una tarea asíncrona dentro
+  // return "Exitooo"; //resolve
+  throw "Algo salio mal" //reject
+}
 
+/*
 ejecutar()
 .then((resultado) => { //van bien
   console.log(resultado)
@@ -13,3 +23,15 @@ ejecutar()
 .catch((error) => {
   console.log(error)
 })
+*/
+
+const ejecuto = async () => {
+  try {
+    //intenta prueba
+    const resultado = await ejecutar(); //espere ⌛
+    console.log(resultado);
+  } catch (error) {
+    console.log(error)
+  }
+}
+ejecuto();
