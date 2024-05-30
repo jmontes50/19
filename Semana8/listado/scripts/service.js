97c284151b103e9e014269cecd6d6eec0147bb49
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-29 20:10:47 -0500

    avance

--- Semana8/listado/scripts/service.js
+++ Semana8/listado/scripts/service.js
@@ -1,11 +1,17 @@
 //Tener el código que me permita obtener los datos
 
 const obtenerUsuarios = async () => {
-  const respuesta = await fetch("https://reqres.in/api/users?page=2");
-  // console.log("RESPUESTA", respuesta);
-  console.log({ respuesta }); // console.log({ respuesta: respuesta });
-}
+  try {
+    const respuesta = await fetch("https://reqres.in/api/users?page=2");
+    // console.log("RESPUESTA", respuesta);
+    console.log({ respuesta }); // console.log({ respuesta: respuesta });
+    const datos = await respuesta.json();
+    console.log(datos);
+    return datos;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-export {
-  obtenerUsuarios
-}
+export { obtenerUsuarios };
