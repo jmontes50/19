8a232aa11477e65f727a20abbd7fa06e27548520
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-29 20:27:20 -0500

    avance

--- Semana8/listado/scripts/app.js
+++ Semana8/listado/scripts/app.js
@@ -3,10 +3,32 @@ import { obtenerUsuarios } from "./service.js";
 const main = async () => {
   try {
     const usuarios = await obtenerUsuarios();
-    console.log(usuarios)
+    console.table(usuarios.data)
   } catch (error) {
     console.log(error)
   }
 };
+
+const Card = (usuario) => {
+  const { first_name, email } = usuario;
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div>
+    <h2>${first_name}</h2>
+    <small>${email}</small>
+  </div>
+  `
+  return div;
+}
+
+const dibujarUsuarios = (usuarios) => {
+  const divRoot = document.getElementById('root');
+  usuarios.forEach((user) => {
+    //la funcion Card me devuelve un div, un elemento
+    const newCard = Card(user);
+    //appendChild añade un elemento como hijo de otro
+    divRoot.appendChild(newCard);
+  });
+}
 
 main();
