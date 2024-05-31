c27ed89e08650fac8dcd6a40f2c9c9468c29159b
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-30 20:52:23 -0500

    avance

--- Semana9/03-listado/src/App.jsx
+++ Semana9/03-listado/src/App.jsx
@@ -2,13 +2,27 @@ import { useState, useEffect } from "react"
 
 const App = () => {
   const [contador, setContador] = useState(0);
+  const [pares, setPares] = useState(2)
+  //aqui arriba haya lógica del componente y puedo hacer operaciones 
+  //declarar variables
+  // justo antes del return
+  console.log("1. lógica de negocio")
+  useEffect(() => {
+    console.log("3. Hola!");
+    // datos condicionales
+  },[])
 
   return (
     <>
+      {console.log("2. dentro del return")}
       <h1>App</h1>
       <h2>Contador: {contador}</h2>
       <button onClick={() => {setContador(contador + 1)}}>
         Incrementar
+      </button>
+      <h2>Pares: {pares}</h2>
+      <button onClick={() => {setPares(pares + 2)}}>
+        Incrementar Pares
       </button>
     </>
   )
