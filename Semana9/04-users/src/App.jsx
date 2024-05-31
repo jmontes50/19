127af0c83005a2016f5c346c1162f7d044bd0a65
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-30 22:21:23 -0500

    avance

--- Semana9/04-users/src/App.jsx
+++ Semana9/04-users/src/App.jsx
@@ -1,26 +1,32 @@
 // al momento de importar ReactStuff, Librerias, Componentes //ESMODULES import export
 import { useState, useEffect } from "react";
-import axios from "axios"; 
+import axios from "axios";
 
 const App = () => {
   const [usuarios, setUsuarios] = useState([]);
 
   useEffect(() => {
-    axios.get('https://reqres.in/api/users?page=2')
-    .then((respuesta) => {
-      console.log(respuesta.status);
-      console.log(respuesta.data.data);
-      setUsuarios(respuesta.data.data);
-      console.log(usuarios);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+    axios
+      .get("https://reqres.in/api/users?page=2")
+      .then((respuesta) => {
+        console.log(respuesta.status);
+        console.log(respuesta.data.data);
+        setUsuarios(respuesta.data.data);
+        console.log(usuarios);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      {/* renderizado de listas, de datos a JSX */}
+      {usuarios.map((user) => (
+        <div>{user.email}</div>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
