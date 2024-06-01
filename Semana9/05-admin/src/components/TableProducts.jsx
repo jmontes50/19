508ded1bfcbfb47de293641a728ed67419894d49
Jorge Montesinos <osmarmontes50@gmail.com>
2024-05-31 22:51:43 -0500

    master

---
+++ Semana9/05-admin/src/components/TableProducts.jsx
@@ -0,0 +1,33 @@
+const TableProducts = (productos) => {
+  return (
+    <>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Color</th>
+            <th>Fecha</th>
+            <th>Precio</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* desestructurando directamente en los argumentos */}
+          {productos.map(({nombre, descripcion, color, fecha, precio, id}) => (
+            <tr key={id}>
+              <td>{nombre}</td>
+              <td>{descripcion}</td>
+              <td>{color}</td>
+              <td>{fecha}</td>
+              <td>S/ {precio}</td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default TableProducts
