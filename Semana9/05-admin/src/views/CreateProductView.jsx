29bd150f8c3478bfc70a42a645a451ae90965aba
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-05 22:29:52 -0500

    avance

--- Semana9/05-admin/src/views/CreateProductView.jsx
+++ Semana9/05-admin/src/views/CreateProductView.jsx
@@ -1,32 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const CreateProductView = () => {
   const [values, setValues] = useState({
-    nombre:"",
-    descripcion:"",
-    precio:0,
-    foto:"https://loremflickr.com/640/480/fashion",
-    color:"ffffff",
-    categoria:"",
-    opiniones:[],
-    fecha: new Date() //hoy
-  })
+    nombre: "",
+    descripcion: "",
+    precio: 0,
+    foto: "https://loremflickr.com/640/480/fashion",
+    color: "ffffff",
+    categoria: "",
+    opiniones: [],
+    fecha: new Date(), //hoy
+  });
 
   const handleValues = (ev) => {
-    console.log(ev.target.name)
+    console.log(ev.target.name);
     const nombrePropiedad = ev.target.name;
-    const valorPropiedad = ev.target.value
+    const valorPropiedad = ev.target.value;
     const newProduct = {
       ...values,
-      [nombrePropiedad]: valorPropiedad
-    }
-    setValues(newProduct)
-  }
-
+      [nombrePropiedad]: valorPropiedad,
+    };
+    setValues(newProduct);
+  };
 
   return (
     <div className="container pt-4">
       <h1 className="mb-4">Crear Producto</h1>
+      {/* nombre */}
       <div className="mb-3">
         <label className="form-label" htmlFor="nombre">
           Nombre
@@ -42,11 +42,33 @@ const CreateProductView = () => {
           onChange={handleValues}
         />
       </div>
+      {/* descripción */}
       <div className="mb-3">
         <label className="form-label" htmlFor="descripcion">
           Descripción
         </label>
-        <textarea className="form-control" id="descripcion"></textarea>
+        <textarea
+          className="form-control"
+          id="descripcion"
+          name="descripcion"
+          value={values.descripcion}
+          onChange={handleValues}
+        ></textarea>
+        {/* precio */}
+        <div className="mb-3">
+          <label className="form-label" htmlFor="precio">
+            Precio
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="precio"
+            placeholder="100"
+            name="precio"
+            value={values.precio}
+            onChange={handleValues}
+          />
+        </div>
       </div>
     </div>
   );
