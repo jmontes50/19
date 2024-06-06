21e3b6a7840efd10cc673317b1f7e19472b49ed3
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-05 22:56:26 -0500

    avance

--- Semana9/05-admin/src/views/CreateProductView.jsx
+++ Semana9/05-admin/src/views/CreateProductView.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { crearProducto } from "../services/productService";
 
 const CreateProductView = () => {
   const [values, setValues] = useState({
@@ -23,96 +24,109 @@ const CreateProductView = () => {
     setValues(newProduct);
   };
 
+  const handleSubmit = async (ev) => {
+    // prevenir la acción por defecto
+    ev.preventDefault();
+    const resultado = await crearProducto(values);
+    console.log(resultado);
+    alert("Producto Creado!")
+  }
+
   return (
     <div className="container pt-4">
       <h1 className="mb-4">Crear Producto</h1>
-      {/* nombre */}
-      <div className="mb-3">
-        <label className="form-label" htmlFor="nombre">
-          Nombre
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="nombre"
-          placeholder="Ej. Vestido Azul"
-          name="nombre"
-          value={values.nombre}
-          // onChange={(ev) => setValues({nombre:ev.target.value})}
-          onChange={handleValues}
-        />
-      </div>
-      {/* descripción */}
-      <div className="mb-3">
-        <label className="form-label" htmlFor="descripcion">
-          Descripción
-        </label>
-        <textarea
-          className="form-control"
-          id="descripcion"
-          name="descripcion"
-          value={values.descripcion}
-          onChange={handleValues}
-        ></textarea>
-        {/* precio */}
+      <form onSubmit={handleSubmit}>
+        {/* nombre */}
         <div className="mb-3">
-          <label className="form-label" htmlFor="precio">
-            Precio
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            id="precio"
-            placeholder="100"
-            name="precio"
-            value={values.precio}
-            onChange={handleValues}
-          />
-        </div>
-        {/* color */}
-        <div className="mb-3">
-          <label className="form-label" htmlFor="color">
-            Color
-          </label>
-          <input
-            type="color"
-            className="form-control"
-            id="color"
-            name="color"
-            value={values.color}
-            onChange={handleValues}
-          />
-        </div>
-        {/* categoria */}
-        <div className="mb-3">
-          <label className="form-label" htmlFor="categoria">
-            Categoría
+          <label className="form-label" htmlFor="nombre">
+            Nombre
           </label>
           <input
             type="text"
             className="form-control"
-            id="categoria"
-            placeholder="Jeans"
-            name="categoria"
-            value={values.categoria}
+            id="nombre"
+            placeholder="Ej. Vestido Azul"
+            name="nombre"
+            value={values.nombre}
+            // onChange={(ev) => setValues({nombre:ev.target.value})}
             onChange={handleValues}
           />
         </div>
-        {/* {fecha} */}
+        {/* descripción */}
         <div className="mb-3">
-          <label className="form-label" htmlFor="fecha">
-            Fecha
+          <label className="form-label" htmlFor="descripcion">
+            Descripción
           </label>
-          <input
-            type="date"
+          <textarea
             className="form-control"
-            id="fecha"
-            name="fecha"
-            value={values.fecha}
+            id="descripcion"
+            name="descripcion"
+            value={values.descripcion}
             onChange={handleValues}
-          />
+          ></textarea>
+          {/* precio */}
+          <div className="mb-3">
+            <label className="form-label" htmlFor="precio">
+              Precio
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="precio"
+              placeholder="100"
+              name="precio"
+              value={values.precio}
+              onChange={handleValues}
+            />
+          </div>
+          {/* color */}
+          <div className="mb-3">
+            <label className="form-label" htmlFor="color">
+              Color
+            </label>
+            <input
+              type="color"
+              className="form-control"
+              id="color"
+              name="color"
+              value={values.color}
+              onChange={handleValues}
+            />
+          </div>
+          {/* categoria */}
+          <div className="mb-3">
+            <label className="form-label" htmlFor="categoria">
+              Categoría
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="categoria"
+              placeholder="Jeans"
+              name="categoria"
+              value={values.categoria}
+              onChange={handleValues}
+            />
+          </div>
+          {/* {fecha} */}
+          <div className="mb-3">
+            <label className="form-label" htmlFor="fecha">
+              Fecha
+            </label>
+            <input
+              type="date"
+              className="form-control"
+              id="fecha"
+              name="fecha"
+              value={values.fecha}
+              onChange={handleValues}
+            />
+          </div>
         </div>
-      </div>
+        <button type="submit" className="btn btn-primary btn-lg">
+          Crear
+        </button>
+      </form>
     </div>
   );
 };
