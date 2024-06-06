f354003b997a58cb268aef84afae76e2330ba279
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-05 22:19:42 -0500

    avance

--- Semana9/05-admin/src/views/CreateProductView.jsx
+++ Semana9/05-admin/src/views/CreateProductView.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react"
+
 const CreateProductView = () => {
+  const [values, setValues] = useState({
+    nombre:"",
+    descripcion:"",
+    precio:0,
+    foto:"https://loremflickr.com/640/480/fashion",
+    color:"ffffff",
+    categoria:"",
+    opiniones:[],
+    fecha: new Date() //hoy
+  })
+
+  const handleValues = (ev) => {
+    console.log(ev.target.name)
+    const nombrePropiedad = ev.target.name;
+    const valorPropiedad = ev.target.value
+    const newProduct = {
+      ...values,
+      [nombrePropiedad]: valorPropiedad
+    }
+    setValues(newProduct)
+  }
+
+
   return (
     <div className="container pt-4">
       <h1 className="mb-4">Crear Producto</h1>
@@ -11,6 +36,10 @@ const CreateProductView = () => {
           className="form-control"
           id="nombre"
           placeholder="Ej. Vestido Azul"
+          name="nombre"
+          value={values.nombre}
+          // onChange={(ev) => setValues({nombre:ev.target.value})}
+          onChange={handleValues}
         />
       </div>
       <div className="mb-3">
