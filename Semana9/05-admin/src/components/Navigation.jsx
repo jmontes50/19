a42164267b606fb9e6c0eec58dabec538c1928a7
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-06 19:41:13 -0500

    avance

--- Semana9/05-admin/src/components/Navigation.jsx
+++ Semana9/05-admin/src/components/Navigation.jsx
@@ -1,12 +1,20 @@
-import { Container, Navbar } from 'react-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Navigation = () => {
   return (
-    <Navbar className='bg-body-tertiary'>
+    <Navbar className='bg-body-tertiary' expand="lg">
         <Container>
             <Navbar.Brand>
                 Admin
             </Navbar.Brand>
+
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className='me-auto'>
+                <Nav.Link>
+                  Crear Producto
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
         </Container>
     </Navbar>
   )
