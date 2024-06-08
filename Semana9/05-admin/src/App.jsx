eab699fb498dc390bc00dda93acda8acb0b40955
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-07 22:02:49 -0500

    avance

--- Semana9/05-admin/src/App.jsx
+++ Semana9/05-admin/src/App.jsx
@@ -1,8 +1,9 @@
 // as es un alias, ProductViewforSaleWithCredit as ProductView
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeView from "./views/HomeView"
-import Navigation from "./components/Navigation"
-import CreateProductView from "./views/CreateProductView"
+import HomeView from "./views/HomeView";
+import Navigation from "./components/Navigation";
+import CreateProductView from "./views/CreateProductView";
+import EditProductView from "./views/EditProductView";
 
 const App = () => {
   return (
@@ -12,10 +13,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/createproduct" element={<CreateProductView />} />
+        {/* al poner en path /:algo estoy indicando que eso sera info que llegara al componente */}
+        <Route path="/editproduct/:id" element={<EditProductView />} />
       </Routes>
       {/* <CreateProductView /> */}
     </Router>
   )
 }
 
-export default App
+export default App;
---
+++ Semana9/05-admin/src/views/EditProductView.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { obtenerProductoPorId } from "../services/productService";
+
+const EditProductView = () => {
+  const [values, setValues] = useState({
+    nombre: "",
+    descripcion: "",
+    precio: 0,
+    foto: "https://loremflickr.com/640/480/fashion",
+    color: "ffffff",
+    categoria: "",
+    opiniones: [],
+    fecha: new Date()
+  })
+
+  //si es que esto es un objeto, lo podemos desestructurar
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const resultado = await obtenerProductoPorId(id);
+        console.log(resultado)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getProduct()
+  }, []);
+
+  return (
+    <div>EditProductView</div>
+  )
+}
+
+export default EditProductView
