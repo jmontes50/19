c499f783b364302784338fe3ce7886602dd179a7
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-12 20:13:29 -0500

    avance

--- Semana9/05-admin/src/views/HomeView.jsx
+++ Semana9/05-admin/src/views/HomeView.jsx
@@ -1,21 +1,45 @@
 import { useEffect, useState } from 'react';
 import TableProducts from '../components/TableProducts';
 import { obtenerProductos, eliminarProducto } from '../services/productService';
+import Swal from "sweetalert2";
 
 const HomeView = () => {
   const [productos, setProductos] = useState([]);
 
-  const handleEliminar = (id) => {
-    console.log("handleEliminar", id)
+  const getProductos = async () => {
+    const prods = await obtenerProductos();
+    setProductos(prods);
+  }
+
+  const handleEliminar = async (id) => {
+    console.log("handleEliminar", id);
+    const resultadoUsuario = await Swal.fire({
+      title:'Desea eliminar el producto?',
+      text:'Esta acción es irreversible 🙀!',
+      confirmButtonText:'Si, deseo eliminarlo',
+      showCancelButton:true,
+      cancelButtonText:'No, no deseo eliminarlo',
+    });
+
+    const { isConfirmed, isDismissed } = resultadoUsuario;
+
+    if(isConfirmed){
+      const resultadoEliminar = await eliminarProducto(id);
+      
+      await Swal.fire({
+        title:"Producto Eliminado",
+        text:"Se elimino exitosamente",
+        icon:"success"
+      })
+      //después de eliminar vuelvo a pedir los productos
+      getProductos();
+
+    }
   }
 
   //la función como argumento en useEffect no puede ser async
   useEffect(() => {
     try {
-      const getProductos = async () => {
-        const prods = await obtenerProductos();
-        setProductos(prods);
-      }
       getProductos();
     } catch (error) {
       console.log(error);
