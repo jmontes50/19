07c5281c456826ff0b4733f7aa52b87b22028696
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-12 23:05:54 -0500

    master

--- Semana11/01-ecommerce/src/hooks/useAxios.js
+++ Semana11/01-ecommerce/src/hooks/useAxios.js
@@ -1,17 +1,31 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useAxios = () => {
+const useAxios = (url, options = { method:'get', data:null }) => {
   const [data, setData] = useState(null); //para los datos
   const [error, setError] = useState(null); //para los errores
   const [loading, setLoading] = useState(false); //para saber si la petición esta en proceso
 
-  const fetchData = async (url, options = { method:'get', data:null }) => {
+  const fetchData = async () => {
     try {
       setLoading(true); //esta cargando
-      const response = await axios({url, ...options});
+      const response = await axios({ url, ...options }); //peticion
+      setData(response.data); //axios me da la info en response.data
     } catch (error) {
-      
+      setError(error);
+    } finally { //que se va ejecutar siempre si o si ya sea la petición tenga éxito o no
+      setLoading(false);
     }
   }
+
+  useEffect(() => {
+    const startFetch = async () => {
+      fetchData()
+    }
+    startFetch();
+  }, [url, options.method, options.data])
+
+  return { data, error, loading }
 }
+
+export default useAxios;
--- Semana11/01-ecommerce/src/views/AllProductView.jsx
+++ Semana11/01-ecommerce/src/views/AllProductView.jsx
@@ -1,6 +1,12 @@
 import ListProducts from "../components/ListProducts";
+import useAxios from "../hooks/useAxios";
 
 const AllProductView = () => {
+
+  const { data, error, loading } = useAxios('https://665a7d66003609eda45e0a13.mockapi.io/productos');
+
+  console.log({ data, error, loading });
+
   return (
     <div>
       <ListProducts />
