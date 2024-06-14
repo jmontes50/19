15762fb157dbfb01b15103dc224c6f0720021b5e
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-13 20:20:01 -0500

    avance

--- Semana11/01-ecommerce/src/components/ProductCard.jsx
+++ Semana11/01-ecommerce/src/components/ProductCard.jsx
@@ -4,20 +4,20 @@ const ProductCard = ({ product }) => {
 
   return (
     <div>
-      <div className="bg-gray-200 p-4 rounded-md">
+      <div className="bg-gray-100 p-4 rounded-md shadow-lg">
         {/* foto */}
         <div className="bg-white rounded-md overflow-hidden shadow-lg max-w-md mx-auto">
-          <img src={ foto } className="w-full h-64 object-cover" alt="" />
+          <img src={ foto } className="w-full h-64 object-cover" alt={ nombre } />
         </div>
         {/* contenido */}
         <div className="p-4">
           <h4 className="text-md font-semibold mb-2">
             { nombre }
           </h4>
-          <p className="text-sm text-gray-800 mb-2 text-ellipsis">
+          <p className="text-sm text-gray-800 mb-2 truncate">
             { descripcion }
           </p>
-          <span className="text-sm text-blue-600">
+          <span className="text-sm font-semibold text-blue-600">
             S/ { precio }
           </span>
         </div>
