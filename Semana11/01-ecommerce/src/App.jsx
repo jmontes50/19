8466754ab77f9d7f974758dd6815b36215f768be
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-13 21:45:45 -0500

    avance

--- Semana11/01-ecommerce/src/App.jsx
+++ Semana11/01-ecommerce/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import AllProductView from "./views/AllProductView";
+import Navbar from "./components/Navbar";
 // import Counter from "./components/Counter";
 
 const App = () => {
   return (
     <Router>
       {/* <Counter /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<AllProductView />} />
       </Routes>
--- Semana11/01-ecommerce/src/components/Navbar.jsx
+++ Semana11/01-ecommerce/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import {
   Transition,
   DisclosureButton,
 } from "@headlessui/react";
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
@@ -12,8 +13,8 @@ const Navbar = () => {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
+              {/* {boton responsive para sm} */}
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* {boton responsive para sm} */}
                 <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Abrir Menú</span>
                   {/* si esta abierto, enseña la X si esta cerrado enseña la 🍔 */}
@@ -23,6 +24,16 @@ const Navbar = () => {
                     <i className="fa-solid fa-bars"></i>
                   )}
                 </DisclosureButton>
+              </div>
+              {/* Links en formato desktop */}
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  {/* {links} */}
+                  <Link to="/" 
+                  className="text-gray-200 hover:bg-gray-700 hover:text-white, rounded-md px-3 py-2 text-sm font-semibold">
+                    Home
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
