e89f4ebc60f51f7cd6228b86df1ddfc8559241b5
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-13 20:28:10 -0500

    avance

--- Semana11/01-ecommerce/src/components/ProductCard.jsx
+++ Semana11/01-ecommerce/src/components/ProductCard.jsx
@@ -1,13 +1,16 @@
 const ProductCard = ({ product }) => {
 
-  const { nombre, foto, descripcion, precio, id } = product;
+  const { nombre, foto, descripcion, precio, id, categoria } = product;
 
   return (
     <div>
       <div className="bg-gray-100 p-4 rounded-md shadow-lg">
         {/* foto */}
-        <div className="bg-white rounded-md overflow-hidden shadow-lg max-w-md mx-auto">
+        <div className="bg-white rounded-md overflow-hidden shadow-lg max-w-md mx-auto relative">
           <img src={ foto } className="w-full h-64 object-cover" alt={ nombre } />
+          <div className="absolute top-2 left-2 bg-blue-500 text-white p-2 text-sm font-semibold rounded-sm">
+            { categoria }
+          </div>
         </div>
         {/* contenido */}
         <div className="p-4">
