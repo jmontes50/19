56a1691dc9fb3696c0407c6357e6aacdbbcb46d2
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-14 22:31:22 -0500

    avance

--- Semana11/01-ecommerce/src/App.jsx
+++ Semana11/01-ecommerce/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { CartContextProvider } from "./context/cartContext";
 import AllProductView from "./views/AllProductView";
+import ProductDetailView from "./views/ProductDetailView";
 import Navbar from "./components/Navbar";
 // import Counter from "./components/Counter";
 
@@ -12,6 +13,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<AllProductView />} />
+          <Route path="/product/:id" element={<ProductDetailView />} />
         </Routes>
       </CartContextProvider>
     </Router>
--- Semana11/01-ecommerce/src/views/ProductDetailView.jsx
+++ Semana11/01-ecommerce/src/views/ProductDetailView.jsx
@@ -4,6 +4,7 @@ const ProductDetailView = () => {
       <section className="text-gray-600 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          {/* contenido izq */}
             <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
                 BRAND NAME
@@ -11,6 +12,7 @@ const ProductDetailView = () => {
               <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
                 Animated Night Hill Illustrations
               </h1>
+              {/* pestañas */}
               <div className="flex mb-4">
                 <a className="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
                   Description
@@ -22,6 +24,7 @@ const ProductDetailView = () => {
                   Details
                 </a>
               </div>
+              {/* descripcion */}
               <p className="leading-relaxed mb-4">
                 Fam locavore kickstarter distillery. Mixtape chillwave tumeric
                 sriracha taximy chia microdosing tilde DIY. XOXO fam inxigo
@@ -29,6 +32,7 @@ const ProductDetailView = () => {
                 seitan poutine tumeric. Gastropub blue bottle austin listicle
                 pour-over, neutra jean.
               </p>
+              {/* especificaciones */}
               <div className="flex border-t border-gray-200 py-2">
                 <span className="text-gray-500">Color</span>
                 <span className="ml-auto text-gray-900">Blue</span>
@@ -41,6 +45,7 @@ const ProductDetailView = () => {
                 <span className="text-gray-500">Quantity</span>
                 <span className="ml-auto text-gray-900">4</span>
               </div>
+              {/*  */}
               <div className="flex">
                 <span className="title-font font-medium text-2xl text-gray-900">
                   $58.00
@@ -48,6 +53,7 @@ const ProductDetailView = () => {
                 <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                   Button
                 </button>
+                {/* 💓 */}
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
                     fill="currentColor"
@@ -62,6 +68,7 @@ const ProductDetailView = () => {
                 </button>
               </div>
             </div>
+            {/* contenido der - img */}
             <img
               alt="ecommerce"
               className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
