dfae20ab6769cc0c5288c11681762cb6508c3c5a
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-19 21:51:51 -0500

    avance

--- Semana11/01-ecommerce/src/context/authContext.jsx
+++ Semana11/01-ecommerce/src/context/authContext.jsx
@@ -1,11 +1,15 @@
 import { createContext, useState } from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../config/firebase";
 
 const AuthContext = createContext();
+
+const googleProvider = GoogleAuthProvider();
 
 const AuthContextProvider = ({ children }) => {
   
   const LoginWithGoogle = () => {
-
+    return signInWithPopup(auth, googleProvider);
   }
 
   return <AuthContext.Provider value={{ LoginWithGoogle }}>
