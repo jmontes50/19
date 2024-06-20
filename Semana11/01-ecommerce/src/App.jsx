97953642d3a84cc637c2669c5ebd85897568c9f0
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-19 21:03:42 -0500

    avance

--- Semana11/01-ecommerce/src/App.jsx
+++ Semana11/01-ecommerce/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { CartContextProvider } from "./context/cartContext";
+import Navbar from "./components/Navbar";
 import AllProductView from "./views/AllProductView";
 import ProductDetailView from "./views/ProductDetailView";
-import Navbar from "./components/Navbar";
+import LoginView from "./views/LoginView";
+
 // import Counter from "./components/Counter";
 
 const App = () => {
@@ -14,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<AllProductView />} />
           <Route path="/product/:id" element={<ProductDetailView />} />
+          <Route path="/login" element={<LoginView />} />
         </Routes>
       </CartContextProvider>
     </Router>
---
+++ Semana11/01-ecommerce/src/views/LoginView.jsx
@@ -0,0 +1,11 @@
+const LoginView = () => {
+  return (
+    <div className="container min-h-96 flex justify-center items-center">
+      <button className="px-5 py-3 bg-blue-500 hover:bg-blue-600 text-white rounded-lg flex items-center text-lg">
+        <i className="fa-brands fa-google me-3 fa-2x"></i>Login
+      </button>
+    </div>
+  );
+};
+
+export default LoginView;
