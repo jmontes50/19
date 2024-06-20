ba16249075187a554cd536912c2b0c7f4a87bd22
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-19 20:14:56 -0500

    master

--- Semana11/01-ecommerce/src/views/ProductDetailView.jsx
+++ Semana11/01-ecommerce/src/views/ProductDetailView.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useAxios from "../hooks/useAxios";
 import CircleColor from "../components/CircleColor";
+import convertDateToLocal from "../utils/parseDate";
 
 const ProductDetailView = () => {
   const { id } = useParams();
@@ -37,15 +38,15 @@ const ProductDetailView = () => {
               </h1>
               {/* pestañas */}
               <div className="flex mb-4">
-                <a className="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
-                  Description
+                <a className="flex-grow text-blue-500 border-b-2 border-blue-500 py-2 text-lg px-1">
+                  Descripción
                 </a>
-                <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
+                {/* <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
                   Reviews
                 </a>
                 <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
                   Details
-                </a>
+                </a> */}
               </div>
               {/* descripcion */}
               <p className="leading-relaxed mb-4">
@@ -58,7 +59,7 @@ const ProductDetailView = () => {
               </div>
               <div className="flex border-t border-gray-200 py-2">
                 <span className="text-gray-500">Fecha</span>
-                <span className="ml-auto text-gray-900">{ fecha }</span>
+                <span className="ml-auto text-gray-900">{ convertDateToLocal(fecha) }</span>
               </div>
               <div className="flex border-t border-b mb-6 border-gray-200 py-2">
                 <span className="text-gray-500">Color</span>
@@ -69,13 +70,13 @@ const ProductDetailView = () => {
               {/*  */}
               <div className="flex">
                 <span className="title-font font-medium text-2xl text-gray-900">
-                  { precio }
+                  S/ { precio }
                 </span>
-                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                  Button
+                <button className="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">
+                  Comprar
                 </button>
                 {/* 💓 */}
-                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -86,7 +87,7 @@ const ProductDetailView = () => {
                   >
                     <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
                   </svg>
-                </button>
+                </button> */}
               </div>
             </div>
             {/* contenido der - img */}
