97ae7f5f4fc9940ead3adec93dc93391ec09b959
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-19 20:53:44 -0500

    master

---
+++ Semana11/01-ecommerce/src/config/firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// auth es una referencia que junto con las config/credenciales nos permite acceder a nuestro módulo de autenticación de firebase
+const auth = getAuth(app);
+
+export {
+    auth
+}
