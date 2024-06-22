1f0b91c0f2e41942b3f976640f24045f0005e4b0
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-21 21:37:11 -0500

    avance

--- Semana11/01-ecommerce/src/views/CartView.jsx
+++ Semana11/01-ecommerce/src/views/CartView.jsx
@@ -6,12 +6,16 @@ const CartView = () => {
   const { cart } = useContext(CartContext);
 
   return (
-    <div className="flex justify-between gap-4 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div className="lg:w-1/2 w-full">
-        <CartTable products={cart} />
+
+      <div className="flex justify-between gap-4 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="lg:w-1/2 w-full">
+        <h2 className="mb-3 text-2xl font-bold p-2 mt-5">Resumen carrito</h2>
+          <CartTable products={cart} />
+        </div>
+        <div className="lg:w-1/2 w-full">
+        <h2 className="mb-3 text-2xl font-bold p-2 mt-5">Confirmar Compra</h2>
+        </div>
       </div>
-      <div className="lg:w-1/2 w-full">Formulario</div>
-    </div>
   );
 };
 
