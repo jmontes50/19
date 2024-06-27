2e0c5ef4fe2f0369f6153d836a19a31eb89d97b4
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-26 20:33:02 -0500

    avance

--- Semana11/01-ecommerce/src/components/Map.jsx
+++ Semana11/01-ecommerce/src/components/Map.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 
 // eslint-disable-next-line react/prop-types
 const Map = ({ height = "400px" }) => {
   const [positionMarker, setPositionMarker] = useState(null);
+  const [coordsMap, setCoordsMap] = useState([-12.0630198, -77.0384351]);
 
   const LocationMarker = ({ position, setPosition }) => {
     const map = useMapEvents({
@@ -28,9 +29,23 @@ const Map = ({ height = "400px" }) => {
     )
   }
 
+  useEffect(() => {
+    // window recuerden no tiene que ser referenciado
+    //para saber si tengo permisos
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((location) => {
+        // console.log("LOCATION: ", location)
+        // const { coords } = location;
+        // const { latitude, longitude } = coords;
+        const { coords: { latitude, longitude } } = location;
+        setCoordsMap([latitude, longitude]);
+      });
+    }
+  }, [])
+
   return (
     <div className="w-full border-2 border-gray-600" style={{ height: height }}>
-      <MapContainer center={[-12.0630198, -77.0384351]} zoom={13}>
+      <MapContainer center={coordsMap} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
