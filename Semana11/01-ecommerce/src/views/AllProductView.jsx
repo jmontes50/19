49b91f27e57d46bc124c5efe6d9543830221a063
Jorge Montesinos <osmarmontes50@gmail.com>
2024-06-27 21:59:54 -0500

    avance

--- Semana11/01-ecommerce/src/views/AllProductView.jsx
+++ Semana11/01-ecommerce/src/views/AllProductView.jsx
@@ -3,6 +3,7 @@ import ListProducts from "../components/ListProducts";
 import useAxios from "../hooks/useAxios";
 import Slider from '@mui/material/Slider';
 import { filterByPrice, orderByName } from "../functions/dataFunctions";
+import Carousel from "../components/Carousel";
 
 const AllProductView = () => {
   // entorno, las variables en VITE con React tienen que ser VITE_ y tenemos que usar import.meta.env.VARIABLE
@@ -28,35 +29,42 @@ const AllProductView = () => {
   }
 
   return (
-    <div className="flex justify-between gap-4 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div className="lg:w-1/4 w-full">
-        <h2 className="font-semibold text-lg mb-3">Filtros</h2>
-        <div className="mb-2">
-          <h3 className="font-semibold mb-2">Filtrar por Precio: </h3>
-          <Slider
-            value={prices}
-            onChange={handleSlider}
-            min={1}
-            max={1000}
-          />
-        </div>
-        <div className="mb-2">
-          <h3 className="font-semibold mb-2">Ordenar: </h3>
-          <select
-            className="p-2 border-2 border-gray-400 rounded w-full"
-            value={orderBy}
-            onChange={(e) => setOrderBy(e.target.value)}
-          >
-            <option value="asc">Ascendente</option>
-            <option value="des">Descendente</option>
-          </select>
-        </div>
-      </div>
-      <div className="lg:w-3/4 w-full">
-        <ListProducts products={productsFiltered} />
-      </div>
+    <>
+      <div className="grid grid-cols-3 grid-rows-2 gap-4 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 max-h-96">
+        <div className="col-span-2 row-span-2">
 
-    </div>
+        </div>
+      </div>
+      <div className="flex justify-between gap-4 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="lg:w-1/4 w-full">
+          <h2 className="font-semibold text-lg mb-3">Filtros</h2>
+          <div className="mb-2">
+            <h3 className="font-semibold mb-2">Filtrar por Precio: </h3>
+            <Slider
+              value={prices}
+              onChange={handleSlider}
+              min={1}
+              max={1000}
+            />
+          </div>
+          <div className="mb-2">
+            <h3 className="font-semibold mb-2">Ordenar: </h3>
+            <select
+              className="p-2 border-2 border-gray-400 rounded w-full"
+              value={orderBy}
+              onChange={(e) => setOrderBy(e.target.value)}
+            >
+              <option value="asc">Ascendente</option>
+              <option value="des">Descendente</option>
+            </select>
+          </div>
+        </div>
+        <div className="lg:w-3/4 w-full">
+          <ListProducts products={productsFiltered} />
+        </div>
+
+      </div>
+    </>
   );
 };
 
