18f621e53f0487062fbaf9c475b3704f40e1e8b5
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-03 22:43:54 -0500

    avance

--- Semana13/firechat/src/components/Chat.jsx
+++ Semana13/firechat/src/components/Chat.jsx
@@ -1,8 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addDocument } from '../app/data/dataSlice';
 
 const Chat = () => {
+  
+  const dispatch = useDispatch();
+
+  const state = useSelector((state) => state.data);
+  console.log("DATA: ", state);
+  
+  const inputNombre = useRef();
+  const inputMensaje = useRef();
+
   return (
-    <div>Chat</div>
+    <div>
+      <input 
+        type="text"
+        placeholder='Nombre'
+        ref={inputNombre}
+        className='form-control'
+      />
+      <textarea
+        ref={inputMensaje}
+        placeholder='Mensaje'
+        className='form-control'
+      ></textarea>
+    </div>
   )
 }
 
