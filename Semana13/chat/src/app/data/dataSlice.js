9a6e39e6675f19d4e5e1ee5c39b8ae457c8cb540
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-03 19:55:06 -0500

    master

--- Semana13/chat/src/app/data/dataSlice.js
+++ Semana13/chat/src/app/data/dataSlice.js
@@ -1,4 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+//createAsyncThunk('nombre para identificar la acción', funcion asincrona)
+const fetchUsers = createAsyncThunk(
+    'users/fetchUsers',
+    //la función asincrona puede recibir 02 argumentos
+    //1ero si es que hay que pasarle alguna información al momento del dispatch, el id de un Usuario, la data de un post
+    //2do es un objeto que me da el createAsyncThunk, que me permite disparar un accion, dar un error, mediante metodos que incluye este objeto
+    async (page = 1, thunkAPI) => {
+        try {
+            const result = await fetch(`https://reqres.in/api/users?page=${page}`)
+            return result.json()
+        } catch (error) {
+            thunkAPI.rejectWithValue(error)
+        }
+    }
+)
 
 const dataSlice = createSlice({
     name: 'users',
