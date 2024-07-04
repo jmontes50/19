2268402f80e65c9798d172638928a5d175e29626
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-03 21:45:46 -0500

    avance

---
+++ Semana13/firechat/src/config/firebaseConfig.js
@@ -0,0 +1,24 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// auth es una referencia que junto con las config/credenciales nos permite acceder a nuestro módulo de autenticación de firebase
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {
+    auth, 
+    db
+}
